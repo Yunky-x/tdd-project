3b265df60b4dcbbf764f8f3cd30f76b92657f95b
Yunky-x <kauasantos070704@gmail.com>
2026-09-09 20:11:37 -0300

    Trabalhando com Múltiplas Listas & API REST

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -27,7 +27,7 @@ class NewVisitorTest(LiveServerTestCase):
                     raise e
                 time.sleep(0.5)
 
-    def test_can_start_a_list_and_retrieve_it_later(self):
+    def test_can_start_a_list_for_one_user(self):
         # Maria decidiu utilizar o novo app TODO. Ela entra em sua página principal:
         self.browser.get(self.live_server_url)
 
@@ -43,12 +43,7 @@ class NewVisitorTest(LiveServerTestCase):
 
         # Ela digita "Estudar testes funcionais" em uma caixa de texto
         inputbox.send_keys('Estudar testes funcionais')
-
-        # Quando ela aperta enter, a página atualiza, e mostra a lista
-        # "1: Estudar testes funcionais" como um item da lista TODO
         inputbox.send_keys(Keys.ENTER)
-        
-        # Agora usamos a nova função de espera no lugar do sleep
         self.wait_for_row_in_list_table('1: Estudar testes funcionais')
 
         # Ainda existe uma caixa de texto convidando para adicionar outro item
@@ -60,3 +55,41 @@ class NewVisitorTest(LiveServerTestCase):
         # A página atualiza novamente, e agora mostra ambos os itens na sua lista
         self.wait_for_row_in_list_table('1: Estudar testes funcionais')
         self.wait_for_row_in_list_table('2: Estudar testes de unidade')
+
+    def test_multiple_users_can_start_lists_at_different_urls(self):
+        # Maria começa uma nova lista
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        inputbox.send_keys('Estudar testes funcionais')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Estudar testes funcionais')
+
+        # Ela nota que sua lista possui uma URL única
+        maria_list_url = self.browser.current_url
+        self.assertRegex(maria_list_url, '/lists/.+')
+
+        # Agora, um novo usuário, João, entra no site
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        # João visita a página inicial. Não existe nenhum sinal da lista de Maria
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element(By.TAG_NAME, 'body').text
+        self.assertNotIn('1: Estudar testes funcionais', page_text)
+        self.assertNotIn('2: Estudar testes de unidade', page_text)
+
+        # João inicia uma nova lista
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        inputbox.send_keys('Comprar leite')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Comprar leite')
+
+        # João pega sua URL única
+        joao_list_url = self.browser.current_url
+        self.assertRegex(joao_list_url, '/lists/.+')
+        self.assertNotEqual(joao_list_url, maria_list_url)
+
+        # Novamente, não existe sinal da lista de Maria
+        page_text = self.browser.find_element(By.TAG_NAME, 'body').text
+        self.assertNotIn('Estudar testes funcionais', page_text)
+        self.assertIn('Comprar leite', page_text)
